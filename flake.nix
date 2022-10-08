cb3dcba104e1b0ea2e8fe071d8080b5924bbc934
Andres Cuberos <andres.cuberos94@gmail.com>
2022-10-08 14:58:01 -0500

    Add utility inputs to flakes
    - Add flake-utils to be able to build for differente architectures.
    - Add mach-nix to manage reproducible python environments.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   description = "REST API useful for keeping track of security vulnerabilities in a system and their treatment status";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    mach-nix.url = github:DavHau/mach-nix;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, flake-utils, mach-nix, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
