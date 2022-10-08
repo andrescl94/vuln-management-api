a74ae98b7b53e7b84f5246113844476967c4dbc8
Andres Cuberos <andres.cuberos94@gmail.com>
2022-10-08 15:48:29 -0500

    Define development shell with required Python dependencies

--- flake.nix
+++ flake.nix
@@ -7,11 +7,25 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
   };
 
-  outputs = { self, flake-utils, mach-nix, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, flake-utils, mach-nix, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          mach-nix-wrapper = import mach-nix {
+            inherit pkgs;
+            pypiDataRev = "207b45139d020d459c8e2f70409668f1559d3e95";
+            pypiDataSha256 = "0w64x47scn0cj854ddnafklljaivv2zigr4zzcvi3b80lfy1ks9f"; 
+            python = "python310";
+          };
+          pkgs = nixpkgs.legacyPackages.${system};
+          pyenv = mach-nix-wrapper.mkPython {
+            python = "python310";
+            requirements = builtins.readFile ./requirements.txt;
+          };
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [pyenv];
+          };
+        }
+    );
 }
