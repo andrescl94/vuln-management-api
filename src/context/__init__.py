dbcf3ea8a138a3c36adcf7461349ed8ab97979b1
Andres Cuberos <andres.cuberos94@gmail.com>
2022-10-10 00:41:58 -0500

    Generate encrypted JWT for authenticated user

--- src/context/__init__.py
+++ src/context/__init__.py
@@ -18,6 +18,8 @@ try:
     APP_ENVIRONMENT = Environment(
         os.environ.get("APP_ENVIRONMENT", "development")
     )
+    JWE_ENCRYPTION_KEY = os.environ["JWE_ENCRYPTION_KEY"]
+    JWT_SIGNING_KEY = os.environ["JWT_SIGNING_KEY"]
     OAUTH_GOOGLE_CLIENT_ID = os.environ["OAUTH_GOOGLE_CLIENT_ID"]
     OAUTH_GOOGLE_SECRET = os.environ["OAUTH_GOOGLE_SECRET"]
 except KeyError as exc:
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,5 +5,15 @@ import pytz
 TZ = pytz.timezone("America/Bogota")
 
 
+def get_from_timestamp(timestamp: float) -> str:
+    return datetime.fromtimestamp(timestamp).astimezone(tz=TZ).replace(
+        microsecond=0
+    ).isoformat()
+
+
 def get_now_as_iso() -> str:
     return datetime.now().astimezone(tz=TZ).replace(microsecond=0).isoformat()
+
+
+def get_now_timestamp() -> float:
+    return datetime.now().astimezone(tz=TZ).timestamp()
