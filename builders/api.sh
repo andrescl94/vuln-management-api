3c46d4893540c3f8137017dd227c453cce827b7f
Andres Cuberos <andres.cuberos94@gmail.com>
2022-10-15 08:13:17 -0500

    Abstract generic builder to reduce duplicate code (#31)

--- builders/api.sh
+++ builders/api.sh
@@ -1,25 +1,29 @@
+#!__bash__
+
 function main {
     export PATH
     export PYTHONPATH
-    local executablePath="/bin/vuln-api"
+    local tempDir
 
-    for input in ${buildInputs[@]}; do
-        PATH="${input}/bin:${PATH}"
-        if [ -d "${input}/lib/python3.10" ]; then
-            PYTHONPATH="${input}/lib/python3.10/site-packages:${PYTHONPATH}"
-        fi
-    done
+    export AWS_ACCESS_KEY_ID="testKey"
+    export AWS_SECRET_ACCESS_KEY="secretKey"
+    export AWS_DEFAULT_REGION="us-east-1"
+    export JWE_ENCRYPTION_KEY='{"k":"ila0UViizq2Aqk7H4Duai-9sYTHbUXkKp0elOFbYTkE","kty":"oct"}'
+    export JWT_SIGNING_KEY="c10c5cde58a9e4396b00f824673bebd5cb9686b4f6ceba197f1d4dbf61e585be5ac366d484831fd60114f95093fc4d76c53bc97035e25cfe31eec20f2d0a8527"
+    export OAUTH_GOOGLE_CLIENT_ID="960572939057-p40kke46pr4acakcnpil52igbtl875h1.apps.googleusercontent.com"
+    export OAUTH_GOOGLE_SECRET="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"
 
-    mkdir -p "${out}/bin" \
-    && cp -r --no-preserve=mode,ownership "${projectSrc}"/* "${out}" \
-    && sed \
-        -e "s#__bash__#${bash}#g" \
-        -e "s#__outPath__#${out}#g" \
-        -e "s#__PATH__#${PATH}#g" \
-        -e "s#__PYTHONPATH__#${PYTHONPATH}#g" \
-        "${entrypoint}" \
-    > "${out}${executablePath}" \
-    && chmod 755 "${out}${executablePath}"
+    PATH="__PATH__" \
+    PYTHONPATH="__PYTHONPATH" \
+    && tempDir=$(mktemp -d) \
+    && cp -r --no-preserve=mode,ownership "__projectSrc__"/* "${tempDir}" \
+    && pushd "${tempDir}" \
+      && PYTHONPATH="${tempDir}/src:${PYTHONPATH}" \
+      && { dynamodb & } \
+      && sleep 10 \
+      && uvicorn --host 0.0.0.0 src.app.main:APP
+
 }
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 main "${@}"
--- builders/dynamodb.sh
+++ builders/dynamodb.sh
@@ -1,20 +1,55 @@
-function main {
-    export PATH
-    local executablePath="/bin/dynamodb-local"
+#!__bash__
 
-    for input in ${buildInputs[@]}; do
-        PATH="${input}/bin:${PATH}"
-    done
+function kill_dynamoDb {
+    local pid
 
-    mkdir -p "${out}/bin" \
-    && sed \
-        -e "s#__bash__#${bash}#g" \
-        -e "s#__dynamoDbZip__#${dynamoDbZip}#g" \
-        -e "s#__src__#${src}#g" \
-        -e "s#__PATH__#${PATH}#g" \
-        "${entrypoint}" \
-    > "${out}${executablePath}" \
-    && chmod 755 "${out}${executablePath}"
+    pid="$(mktemp)" \
+    && if ! lsof -t -i:8022 > "${pid}"; then
+        echo "DynamoDB instance is not currently running"
+    fi \
+    && while read -r process; do
+        if kill -9 "${process}"; then
+            echo "Killing running DynamoDB instance"
+        else
+            echo "Unable to terminate DynamoDB instance" \
+            && return 1
+        fi \
+        && sleep 1
+    done < "${pid}"
 }
 
+function main {
+    export AWS_ACCESS_KEY_ID="testKey"
+    export AWS_SECRET_ACCESS_KEY="testSecretKey"
+    export PATH
+    local infraSrc
+    local tempDir
+
+    PATH="__PATH__" \
+    && infraSrc="__src__" \
+    && tempDir="$(mktemp -d)" \
+    && cp -r --no-preserve=mode,ownership "${infraSrc}" "${tempDir}/terraform" \
+    && pushd "${tempDir}" \
+        && echo "Terminating existing instances of DynamoDb..." \
+        && kill_dynamoDb \
+        && echo "Unpacking DynamoDb..." \
+        && unzip -u "__dynamoDbZip__" \
+        && echo "Launching DynamoDb..." \
+        && {
+            java \
+                -Djava.library.path=./DynamoDBLocal_lib \
+                -jar DynamoDBLocal.jar \
+                -inMemory \
+                --port 8022 \
+                -sharedDb &
+        } \
+    && popd \
+    && pushd "${tempDir}/terraform" \
+        && terraform init \
+        && terraform apply -auto-approve \
+    && popd \
+    && wait
+}
+
+trap kill_dynamoDb EXIT
 main "${@}"
